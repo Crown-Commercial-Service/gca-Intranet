2ba2d7fe542361197ec8794e5dbac28f1a6f5902
Adam Wynn <adam.wynn@crowncommercial.gov.uk>
2026-02-17 10:35:55 +0000

    Fix logo placement under 1600px and load child CSS overrides

--- wp-content/themes/gca-intranet/functions.php
+++ wp-content/themes/gca-intranet/functions.php
@@ -1,16 +1,13 @@
 <?php
-if (!defined('ABSPATH')) {
-	exit;
-}
+if (!defined("ABSPATH")) { exit; }
 
-/**
- * Enqueue parent theme styles
- */
-add_action('wp_enqueue_scripts', function () {
-	wp_enqueue_style(
-		'gca-intranet-foundation',
-		get_template_directory_uri() . '/style.css',
-		[],
-		wp_get_theme(get_template())->get('Version')
-	);
-});
+add_action("wp_enqueue_scripts", function () {
+
+  wp_enqueue_style(
+    "gca-intranet-child",
+    get_stylesheet_directory_uri() . "/style.css",
+    [],
+    filemtime(get_stylesheet_directory() . "/style.css")
+  );
+
+}, 999);
