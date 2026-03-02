6c9d3e84967992837f8446e683eb460a075e5ed6
Jatin Patel <jatin.patel@crowncommercial.gov.uk>
2026-03-02 09:21:06 +0000

    Add mega menu and tidy up

--- wp-content/themes/gca-intranet-foundation/header.php
+++ wp-content/themes/gca-intranet-foundation/header.php
@@ -1,12 +1,6 @@
 <?php
 /**
- * Header template
- * Desktop:
- *  - Logo sits visually to the LEFT of the boxed container content
- *  - Boxed content is 2 equal-height rows: (top) utility+search, (bottom) nav
- * Mobile:
- *  - Row 1: logo left | utility right
- *  - Row 2: search left | toggle right
+ * Header template - Pure GDS/CCS Responsive Structure
  */
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
@@ -16,113 +10,93 @@
   <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class('govuk-template__body'); ?>>
 <?php wp_body_open(); ?>
 
 <header class="site-header" role="banner">
   <a href="#main-content" class="govuk-skip-link" data-module="govuk-skip-link">Skip to main content</a>
+
   <div class="bg-white gca-header-bg">
-    <div class="container-xxl pt-3">
-      <div class="gca-header-shell">
+    <div class="ccs-width-container govuk-!-padding-top-3">
 
-        <!-- Top row -->
-        <div class="gca-header-topbar">
+      <div class="gca-header-main-row">
 
-          <!-- Logo (positioned outside the boxed layout on desktop via CSS) -->
-          <div class="gca-header-logo-col">
-            <div class="site-branding">
-              <?php
-                if (function_exists('the_custom_logo') && has_custom_logo()) {
-                  the_custom_logo();
-                } else {
-                  $logo_rel = '/assets/img/Government-Commercial-Agency.svg';
-                  $logo_url = file_exists(get_stylesheet_directory() . $logo_rel)
-                    ? get_stylesheet_directory_uri() . $logo_rel
-                    : get_template_directory_uri() . $logo_rel;
-                  ?>
-                  <a class="custom-logo-link"
-                     href="<?php echo esc_url(home_url('/')); ?>"
-                     rel="home"
-                     aria-label="<?php echo esc_attr(get_bloginfo('name')); ?>">
-                    <img
-                      class="gca-header-logo"
-                      src="<?php echo esc_url($logo_url); ?>"
-                      alt="<?php echo esc_attr(get_bloginfo('name')); ?>"
-                    />
-                  </a>
-                  <?php
-                }
-              ?>
-            </div>
-          </div>
-
-          <nav class="utility-nav" aria-label="Utility navigation">
-            <ul class="nav">
-              <li class="nav-item">
-                <a class="nav-link" href="<?php echo esc_url(get_theme_mod('gca_definition_finder_url', '#')); ?>">
-                  Definition finder
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="<?php echo esc_url(get_theme_mod('gca_staff_directory_url', '#')); ?>">
-                  Staff directory
-                </a>
-              </li>
-            </ul>
-          </nav>
-
-          <form class="site-search" role="search"
-                action="<?php echo esc_url(get_theme_mod('gca_search_url', home_url('/'))); ?>"
-                method="get">
-            <label class="visually-hidden" for="site-search">Search the intranet</label>
-            <div class="input-group">
-              <input id="site-search" name="s" type="search"
-                     class="form-control"
-                     placeholder="Search the intranet"
-                     autocomplete="off">
-              <button class="btn btn-primary" type="submit" aria-label="Search">
-                <span class="visually-hidden">Search</span>
-                <svg class="gca-search-icon" width="18" height="18" viewBox="0 0 16 16" aria-hidden="true" focusable="false">
-                  <path fill="currentColor" d="M11.742 10.344l3.387 3.387-.998.998-3.387-3.387a6 6 0 1 1 .998-.998zM6.5 11.5a5 5 0 0 0 0-10 5 5 0 0 0 0 10z"></path>
-                </svg>
-              </button>
-            </div>
-          </form>
-
-          <!-- Hamburger (mobile only) -->
-          <button class="navbar-toggler gca-header-toggler" type="button"
-                  data-bs-toggle="collapse" data-bs-target="#primaryNav"
-                  aria-controls="primaryNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon" aria-hidden="true"></span>
-          </button>
-
-        </div>
-
-        <!-- Bottom row: Primary nav -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-white gca-header-nav" aria-label="Primary navigation">
-          <div id="primaryNav" class="collapse navbar-collapse">
+        <div class="gca-header-logo-col">
+          <div class="site-branding">
             <?php
-              $args = [
-                'theme_location' => 'primary',
-                'container'      => false,
-                'menu_class'     => 'navbar-nav me-auto mb-2 mb-lg-0',
-                'fallback_cb'    => false,
-                'item_spacing'   => 'discard',
-                'depth'          => 2,
-              ];
-
-              if (class_exists('GCA_Bootstrap_5_Navwalker')) {
-                $args['walker'] = new GCA_Bootstrap_5_Navwalker();
+              if (function_exists('the_custom_logo') && has_custom_logo()) {
+                the_custom_logo();
               } else {
-                $args['depth'] = 1;
+                $logo_rel = '/assets/img/Government-Commercial-Agency.svg';
+                $logo_url = file_exists(get_stylesheet_directory() . $logo_rel)
+                  ? get_stylesheet_directory_uri() . $logo_rel
+                  : get_template_directory_uri() . $logo_rel;
+                ?>
+                <a class="custom-logo-link" href="<?php echo esc_url(home_url('/')); ?>" rel="home" aria-label="<?php echo esc_attr(get_bloginfo('name')); ?>">
+                  <img class="gca-header-logo" src="<?php echo esc_url($logo_url); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" />
+                </a>
+                <?php
               }
-
-              wp_nav_menu($args);
             ?>
           </div>
-        </nav>
+        </div>
 
+        <div class="gca-header-actions-col">
+
+          <div class="gca-header-topbar-inner">
+
+            <nav class="utility-nav" aria-label="Utility navigation">
+              <ul class="utility-nav-list">
+                <li>
+                  <a class="govuk-link" href="<?php echo esc_url(get_theme_mod('gca_definition_finder_url', '#')); ?>">Definition finder</a>
+                </li>
+                <li>
+                  <a class="govuk-link" href="<?php echo esc_url(get_theme_mod('gca_staff_directory_url', '#')); ?>">Staff directory</a>
+                </li>
+              </ul>
+            </nav>
+
+            <form class="site-search" role="search" action="<?php echo esc_url(get_theme_mod('gca_search_url', home_url('/'))); ?>" method="get">
+              <label class="govuk-visually-hidden" for="site-search">Search the intranet</label>
+              <div class="search-input-group">
+                <input id="site-search" name="s" type="search" class="govuk-input" placeholder="Search the intranet" autocomplete="off">
+                <button class="govuk-button search-submit" type="submit" aria-label="Search">
+                  <span class="govuk-visually-hidden">Search</span>
+                  <svg class="gca-search-icon" width="20" height="20" viewBox="0 0 16 16" aria-hidden="true" focusable="false">
+                    <path fill="currentColor" d="M11.742 10.344l3.387 3.387-.998.998-3.387-3.387a6 6 0 1 1 .998-.998zM6.5 11.5a5 5 0 0 0 0-10 5 5 0 0 0 0 10z"></path>
+                  </svg>
+                </button>
+              </div>
+            </form>
+
+            <button class="global-navigation__toggler" type="button" aria-controls="primaryNav" aria-expanded="false" aria-label="Toggle navigation">
+              Menu
+            </button>
+
+          </div>
+        </div>
+
+      </div> <div class="nav-wrapper govuk-width-container">
+        <nav class="global-navigation" id="primaryNav" aria-label="Primary navigation">
+        <?php
+          $args = [
+            'theme_location' => 'primary',
+            'container'      => false,
+            'menu_class'     => 'nav-list nav-list--primary',
+            'fallback_cb'    => false,
+            'item_spacing'   => 'discard',
+            'depth'          => 2,
+          ];
+
+          if (class_exists('CCS_Mega_Menu_Walker')) {
+            $args['walker'] = new CCS_Mega_Menu_Walker();
+          }
+
+          wp_nav_menu($args);
+        ?>
+        </nav>
       </div>
+
     </div>
   </div>
 </header>
---
+++ wp-content/themes/gca-intranet-foundation/inc/class-ccs-mega-menu-walker.php
@@ -0,0 +1,83 @@
+<?php
+class CCS_Mega_Menu_Walker extends Walker_Nav_Menu {
+    // 1. Wrap the sub-menu in our mega-menu div
+    public function start_lvl( &$output, $depth = 0, $args = null ) {
+        $indent = str_repeat("\t", $depth);
+        if ( $depth === 0 ) {
+            // Top level dropdown gets the mega menu wrapper
+            $output .= "\n$indent<div class=\"mega-menu-wrapper\"><ul class=\"sub-menu\">\n";
+        } else {
+            // Deeper levels (if any) just get standard ul
+            $output .= "\n$indent<ul class=\"sub-menu\">\n";
+        }
+    }
+
+    // 2. Close the mega-menu div
+    public function end_lvl( &$output, $depth = 0, $args = null ) {
+        $indent = str_repeat("\t", $depth);
+        if ( $depth === 0 ) {
+            $output .= "$indent</ul></div>\n";
+        } else {
+            $output .= "$indent</ul>\n";
+        }
+    }
+
+    // 3. Add chevrons and ARIA attributes to items with children
+    public function start_el( &$output, $item, $depth = 0, $args = null, $id = 0 ) {
+        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        
+        // Check if this item has children
+        $has_children = in_array( 'menu-item-has-children', $classes );
+
+        // Add custom classes for our CSS
+        if ( $has_children && $depth === 0 ) {
+            $classes[] = 'has-mega-menu';
+        }
+        
+        // Add active state class matching your design
+        if ( in_array('current-menu-item', $classes) || in_array('current-menu-ancestor', $classes) ) {
+            $classes[] = 'active';
+        }
+
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+        $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+        $output .= $indent . '<li' . $class_names . '>';
+
+        $atts = array();
+        $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+        $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+        $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+        $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+        if ( $has_children && $depth === 0 ) {
+            $atts['aria-expanded'] = 'false'; // Accessibility for dropdowns
+            $atts['aria-haspopup'] = 'true';
+        }
+
+        $attributes = '';
+        foreach ( $atts as $attr => $value ) {
+            if ( ! empty( $value ) ) {
+                $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+                $attributes .= ' ' . $attr . '="' . $value . '"';
+            }
+        }
+
+        $title = apply_filters( 'the_title', $item->title, $item->ID );
+        
+        $item_output = $args->before;
+        $item_output .= '<a' . $attributes . '>';
+        $item_output .= $args->link_before . $title . $args->link_after;
+        
+        // Append a chevron SVG if it has a dropdown
+        if ( $has_children && $depth === 0 ) {
+            $item_output .= ' <svg class="nav-chevron" width="12" height="8" viewBox="0 0 12 8" aria-hidden="true" style="margin-left: 8px; vertical-align: middle;"><path fill="currentColor" d="M1.41 0L6 4.58 10.59 0 12 1.41l-6 6-6-6z"/></svg>';
+        }
+        
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+}
