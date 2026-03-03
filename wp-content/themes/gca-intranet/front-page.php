d6954cceb21eb22d7ffa0bc7ab138eb61cabc2d4
Adam Wynn <adam.wynn@crowncommercial.gov.uk>
2026-03-03 14:07:23 +0000

    GI-100: Remove Take a look inner wrapper to prevent double panel

--- wp-content/themes/gca-intranet/front-page.php
+++ wp-content/themes/gca-intranet/front-page.php
@@ -174,42 +174,42 @@ get_header();
           </div>
 
           <?php if ($take_href !== '') : ?>
-            <div class="gca-take-a-look" data-testid="take-a-look-card">
-              <a class="gca-take-a-look__link govuk-link"
-                data-testid="take-a-look-link"
-                href="<?php echo $take_href; ?>">
-                <span class="gca-take-a-look__content">
-                  <p class="govuk-body gca-take-a-look__text govuk-!-margin-bottom-0">
-                    <?php echo esc_html($take_text); ?>
-                  </p>
-                </span>
+            <!-- Single card: no outer wrapper, no inner content wrapper -->
+            <a class="gca-take-a-look__link govuk-link"
+              data-testid="take-a-look-link"
+              href="<?php echo $take_href; ?>">
 
-                <span class="gca-take-a-look__icon" aria-hidden="true">
-                  <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M32 16C32 12.8355 31.0616 9.74206 29.3035 7.11088C27.5454 4.47969 25.0466 2.42893 22.1229 1.21793C19.1993 0.00692534 15.9823 -0.309928 12.8786 0.307436C9.77486 0.924799 6.92393 2.44865 4.68629 4.68629C2.44865 6.92393 0.924799 9.77486 0.307435 12.8786C-0.309928 15.9823 0.00692538 19.1993 1.21793 22.1229C2.42893 25.0466 4.47969 27.5454 7.11088 29.3035C9.74206 31.0616 12.8355 32 16 32L16 16H32Z" fill="#9CAF27"/>
-                    <path d="M22 22L31.3802 31.5833M31.3802 31.5833V22M31.3802 31.5833H22" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                  </svg>
-                </span>
-              </a>
-            </div>
+              <p class="govuk-body gca-take-a-look__text govuk-!-margin-bottom-0">
+                <?php echo esc_html($take_text); ?>
+              </p>
+
+              <span class="gca-take-a-look__icon" aria-hidden="true">
+                <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M32 16C32 12.8355 31.0616 9.74206 29.3035 7.11088C27.5454 4.47969 25.0466 2.42893 22.1229 1.21793C19.1993 0.00692534 15.9823 -0.309928 12.8786 0.307436C9.77486 0.924799 6.92393 2.44865 4.68629 4.68629C2.44865 6.92393 0.924799 9.77486 0.307435 12.8786C-0.309928 15.9823 0.00692538 19.1993 1.21793 22.1229C2.42893 25.0466 4.47969 27.5454 7.11088 29.3035C9.74206 31.0616 12.8355 32 16 32L16 16H32Z" fill="#9CAF27"/>
+                  <path d="M22 22L31.3802 31.5833M31.3802 31.5833V22M31.3802 31.5833H22" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+              </span>
+            </a>
+
           <?php else : ?>
-            <div class="gca-take-a-look" data-testid="take-a-look-card">
-              <div class="gca-take-a-look__link" aria-label="<?php echo esc_attr__('Take a look not configured', 'gca-intranet'); ?>">
-                <span class="gca-take-a-look__content">
-                  <p class="govuk-body gca-take-a-look__text govuk-!-margin-bottom-0">
-                    <?php echo esc_html($take_text); ?>
-                  </p>
-                </span>
+            <!-- Single card: no outer wrapper, no inner content wrapper -->
+            <div class="gca-take-a-look__link"
+              data-testid="take-a-look-link"
+              aria-label="<?php echo esc_attr__('Take a look not configured', 'gca-intranet'); ?>">
 
-                <span class="gca-take-a-look__icon" aria-hidden="true">
-                  <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M32 16C32 12.8355 31.0616 9.74206 29.3035 7.11088C27.5454 4.47969 25.0466 2.42893 22.1229 1.21793C19.1993 0.00692534 15.9823 -0.309928 12.8786 0.307436C9.77486 0.924799 6.92393 2.44865 4.68629 4.68629C2.44865 6.92393 0.924799 9.77486 0.307435 12.8786C-0.309928 15.9823 0.00692538 19.1993 1.21793 22.1229C2.42893 25.0466 4.47969 27.5454 7.11088 29.3035C9.74206 31.0616 12.8355 32 16 32L16 16H32Z" fill="#9CAF27"/>
-                    <path d="M22 22L31.3802 31.5833M31.3802 31.5833V22M31.3802 31.5833H22" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                  </svg>
-                </span>
-              </div>
+              <p class="govuk-body gca-take-a-look__text govuk-!-margin-bottom-0">
+                <?php echo esc_html($take_text); ?>
+              </p>
+
+              <span class="gca-take-a-look__icon" aria-hidden="true">
+                <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M32 16C32 12.8355 31.0616 9.74206 29.3035 7.11088C27.5454 4.47969 25.0466 2.42893 22.1229 1.21793C19.1993 0.00692534 15.9823 -0.309928 12.8786 0.307436C9.77486 0.924799 6.92393 2.44865 4.68629 4.68629C2.44865 6.92393 0.924799 9.77486 0.307435 12.8786C-0.309928 15.9823 0.00692538 19.1993 1.21793 22.1229C2.42893 25.0466 4.47969 27.5454 7.11088 29.3035C9.74206 31.0616 12.8355 32 16 32L16 16H32Z" fill="#9CAF27"/>
+                  <path d="M22 22L31.3802 31.5833M31.3802 31.5833V22M31.3802 31.5833H22" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+              </span>
             </div>
           <?php endif; ?>
+
         </div>
       <?php endif; ?>
 
