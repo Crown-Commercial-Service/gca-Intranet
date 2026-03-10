9f8e0ea833157d5bc9bd190ca33688e64853e44f
osamamunir-DP <osama.munira@digital-partner.uk>
2026-03-10 21:48:09 +0000

    'Published by' Component

---
+++ wp-content/themes/gca-intranet-foundation/template-parts/published-by.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Usage:
+ * get_template_part('template-parts/published-by');
+ */
+
+$author        = get_the_author();
+$modified_date = get_the_modified_date('j F Y');
+
+if (!$author && !$modified_date) {
+  return;
+}
+?>
+
+<div class="gca-published-by" data-testid="published-by">
+  <hr class="gca-published-by__divider" aria-hidden="true">
+  <div class="gca-published-by__meta govuk-body-s">
+    <?php if ($author) : ?>
+      <span class="gca-published-by__author" data-testid="published-by-author">
+        By <?php echo esc_html($author); ?>
+      </span>
+    <?php endif; ?>
+
+    <?php if ($modified_date) : ?>
+      <span class="gca-published-by__date" data-testid="published-by-date">
+        Last updated <?php echo esc_html($modified_date); ?>
+      </span>
+    <?php endif; ?>
+  </div>
+</div>
