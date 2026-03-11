465f65e782640dbd3fe42226519e41ff9ee8c91f
Jatin Patel <jatin.patel@crowncommercial.gov.uk>
2026-03-11 14:58:02 +0000

    Update logic to use a test db

--- docker/wp-config-extra.php
+++ docker/wp-config-extra.php
@@ -32,3 +32,15 @@ if (!defined('WP_MEMORY_LIMIT')) {
 if (!defined('WP_MAX_MEMORY_LIMIT')) {
     define('WP_MAX_MEMORY_LIMIT', '1024M');
 }
+
+/**
+ * Playwright Test Database Switcher
+ */
+if (isset($_SERVER['HTTP_X_GCA_TEST_SUITE']) && $_SERVER['HTTP_X_GCA_TEST_SUITE'] === 'true') {
+    // If DB_NAME isn't defined yet, set it to the test DB
+    if (!defined('DB_NAME')) {
+        define('DB_NAME', 'wordpress-test');
+    }
+}
+
+define( 'WP_ENVIRONMENT_TYPE', 'local' );
