50b6803b1af985473f6fa1f6981ddb5916c76483
Adam Wynn <adam.wynn@crowncommercial.gov.uk>
2026-03-23 13:14:06 +0000

    Add Google Tag Manager to UAT environment

--- wp-content/themes/gca-intranet-foundation/functions.php
+++ wp-content/themes/gca-intranet-foundation/functions.php
@@ -772,3 +772,22 @@ add_filter('get_the_archive_title', function ($title) {
     }
     return $title;
 });
+add_action('wp_head', function () {
+
+    // IMPORTANT: Only load GTM outside of development
+    if (wp_get_environment_type() === 'development') {
+        return;
+    }
+
+    ?>
+    <!-- Google Tag Manager -->
+    <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-P9TZ7BPX');
+    </script>
+    <!-- End Google Tag Manager -->
+    <?php
+});
