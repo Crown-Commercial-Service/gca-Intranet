dc6a394e3e104dfb6e3a9712f9a52c24f3f369bc
Jatin Patel <jatin.patel@crowncommercial.gov.uk>
2026-03-24 11:20:44 +0000

    Changed the name of the classes to avoid clashes

--- wp-content/themes/gca-intranet-foundation/single-blog.php
+++ wp-content/themes/gca-intranet-foundation/single-blog.php
@@ -13,7 +13,7 @@ get_template_part('template-parts/breadcrumbs');
 
 
 
-<div class="govuk-width-container blogs" data-testid="blog-container">
+<div class="govuk-width-container blogs-container" data-testid="blog-container">
     <main class="govuk-main-wrapper" id="main-content" data-testid="blog-main">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 
