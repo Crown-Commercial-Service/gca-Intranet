cd772b150d91eb90811c21557ed4ddfa667c6ba9
Adam Wynn <adam.wynn@crowncommercial.gov.uk>
2026-03-27 13:42:45 +0000

    Fix category and location pill colours

--- wp-content/themes/gca-intranet-foundation/archive-event.php
+++ wp-content/themes/gca-intranet-foundation/archive-event.php
@@ -53,9 +53,11 @@ get_template_part('template-parts/breadcrumbs');
                     $visible_i = 0;
                     foreach ($event_categories as $cat) :
                       if (strtolower($cat->name) === 'uncategorized' || strtolower($cat->name) === 'uncategorised') continue; ?>
-                      <span class="tag_label <?php echo $visible_i === 0 ? 'green' : 'grey'; ?> govuk-body-s" data-testid="archive-event-post-category">
+                      
+                      <span class="tag_label <?php echo $visible_i === 0 ? '' : 'grey'; ?> govuk-body-s" data-testid="archive-event-post-category">
                         <?php echo esc_html($cat->name); ?>
                       </span>
+
                       <?php $visible_i++;
                     endforeach;
                   endif; ?>
@@ -64,9 +66,11 @@ get_template_part('template-parts/breadcrumbs');
                   $event_locations = get_the_terms(get_the_ID(), 'event_location');
                   if ($event_locations && !is_wp_error($event_locations)) :
                     foreach ($event_locations as $location) : ?>
-                      <span class="tag_label govuk-body-s" data-testid="archive-event-post-location">
+                      
+                      <span class="tag_label grey govuk-body-s" data-testid="archive-event-post-location">
                         <?php echo esc_html($location->name); ?>
                       </span>
+
                     <?php endforeach;
                   endif; ?>
                 </div>
