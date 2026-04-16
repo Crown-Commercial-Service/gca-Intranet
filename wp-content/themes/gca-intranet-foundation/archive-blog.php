f1c7f531d1b4551e496d811d808b64a1e7a1a19c
Adam Wynn <adam.wynn@crowncommercial.gov.uk>
2026-04-16 14:20:27 +0100

    WIP: GI-209 filter component styling + behaviour

--- wp-content/themes/gca-intranet-foundation/archive-blog.php
+++ wp-content/themes/gca-intranet-foundation/archive-blog.php
@@ -12,84 +12,194 @@ get_template_part('template-parts/breadcrumbs');
 ?>
 
 <div class="govuk-width-container" data-testid="blog-container">
-  <main class="govuk-main-wrapper" id="main-content" tabindex="-1" data-testid="blog-main">
+  <main class="govuk-main-wrapper" id="main-content" tabindex="-1">
 
-    <?php if (have_posts()) : ?>
+    <div class="govuk-grid-row">
 
-      <?php while (have_posts()) : the_post(); ?>
-        <article class="blog-box" data-testid="blog-post">
+      <!-- FILTERS -->
+      <div class="govuk-grid-column-one-quarter">
 
-          <div class="blog_profile_img" >
+        <?php
+        $selected_labels = isset($_GET['label'])
+          ? array_map('sanitize_text_field', (array) $_GET['label'])
+          : [];
+
+        $filter_terms = get_terms([
+          'taxonomy'   => 'label',
+          'hide_empty' => true,
+        ]);
+
+        $view_all_checked = empty($selected_labels) ? 'checked' : '';
+        ?>
+
+        <form method="get" class="gca-filters">
+
+          <!-- HEADER -->
+          <div class="gca-filters__header">
+            <h2 class="govuk-heading-m">Apply filters</h2>
+
+            <?php if (!empty($selected_labels)) : ?>
+              <a href="<?php echo esc_url(get_post_type_archive_link('blog')); ?>" class="govuk-link govuk-body-s">
+                Clear filters
+              </a>
+            <?php endif; ?>
+          </div>
+
+          <!-- GOV ACCORDION (FIXED) -->
+          <div 
+            class="govuk-accordion gca-filter-card" 
+            data-module="govuk-accordion"
+            data-show-all-sections="false"
+            data-show-all-text="false"
+            id="filters-accordion"
+          >
+
+            <div class="govuk-accordion__section govuk-accordion__section--expanded">
+
+              <!-- HEADER -->
+              <div class="govuk-accordion__section-header">
+                <h3 class="govuk-accordion__section-heading">
+                  <button
+                    type="button"
+                    class="govuk-accordion__section-button gca-filter-card__title"
+                    id="filter-heading-type"
+                  >
+                    Type of article
+                  </button>
+                </h3>
+              </div>
+
+              <!-- CONTENT -->
+              <div
+                id="filter-content-type"
+                class="govuk-accordion__section-content gca-filter-card__content"
+                aria-labelledby="filter-heading-type"
+              >
+
+                <div class="govuk-checkboxes" data-module="govuk-checkboxes">
+
+                  <!-- VIEW ALL -->
+                  <div class="govuk-checkboxes__item govuk-checkboxes__item--small">
+                    <input
+                      class="govuk-checkboxes__input govuk-checkboxes__input--small"
+                      id="view-all"
+                      type="checkbox"
+                      <?php echo $view_all_checked; ?>
+                      onclick="window.location.href='<?php echo esc_url(get_post_type_archive_link('blog')); ?>'"
+                    >
+                    <label class="govuk-label govuk-checkboxes__label" for="view-all">
+                      View all
+                    </label>
+                  </div>
+
+                  <!-- TERMS -->
+                  <?php foreach ($filter_terms as $term) : ?>
+                    <?php $checked = in_array($term->slug, $selected_labels) ? 'checked' : ''; ?>
+
+                    <div class="govuk-checkboxes__item govuk-checkboxes__item--small">
+                      <input
+                        class="govuk-checkboxes__input govuk-checkboxes__input--small"
+                        id="term-<?php echo esc_attr($term->slug); ?>"
+                        name="label[]"
+                        type="checkbox"
+                        value="<?php echo esc_attr($term->slug); ?>"
+                        <?php echo $checked; ?>
+                        onchange="this.form.submit();"
+                      >
+                      <label
+                        class="govuk-label govuk-checkboxes__label"
+                        for="term-<?php echo esc_attr($term->slug); ?>"
+                      >
+                        <?php echo esc_html($term->name); ?>
+                      </label>
+                    </div>
+
+                  <?php endforeach; ?>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </form>
+
+      </div>
+
+      <!-- RESULTS -->
+      <div class="govuk-grid-column-three-quarters">
+
+        <?php if (have_posts()) : ?>
+
+          <?php while (have_posts()) : the_post(); ?>
+            <article class="blog-box">
+
+              <div class="blog_profile_img">
+                <?php 
+                  $custome_author_img = get_field('image'); 
+                  
+                  if ($custome_author_img) : 
+                      echo wp_get_attachment_image($custome_author_img, 'thumbnail', false, ['class' => 'avatar']); 
+                  else : 
+                      echo get_avatar(get_the_author_meta('ID'));
+                  endif; 
+                ?>
+              </div>
+
+              <div>
+                <h2 class="govuk-heading-m govuk-!-margin-bottom-2">
+                  <a class="govuk-link" href="<?php the_permalink(); ?>">
+                    <?php the_title(); ?>
+                  </a>
+                </h2>
+
+                <p>
+                  <?php echo esc_html(gca_clean_post_excerpt(320)); ?>
+                </p>
+
+                <p class="govuk-body">
+                  By <?php echo esc_html(get_the_author()); ?>
+                </p>
+                
+                <div class="date_bottom">
+                  <span>
+                    <?php echo esc_html(get_the_date('j F Y')); ?>
+                  </span>
+
+                  <?php 
+                  $post_terms = get_the_terms(get_the_ID(), 'label');
+
+                  if ($post_terms && !is_wp_error($post_terms)) : 
+                    $term = array_shift($post_terms); ?>
+                    <span class="govuk-body-s tag_label location">
+                        <?php echo esc_html($term->name); ?>
+                    </span>
+                  <?php endif; ?>
+                </div>
+              </div>
+
+            </article>
+          <?php endwhile; ?>
+
+          <div class="govuk-!-margin-top-8 govuk-!-margin-bottom-8">
             <?php 
-              $custome_author_img = get_field('image'); 
-              
-              if ($custome_author_img) : 
-                  echo wp_get_attachment_image($custome_author_img, 'thumbnail', false, ['class' => 'avatar']); 
-              else : 
-                  if ($avatar = get_avatar(get_the_author_meta('ID'))) :
-                      echo $avatar;
-                  endif;
-              endif; 
+              the_posts_pagination([
+                'mid_size'  => 2,
+                'prev_text' => '<span>Previous</span>',
+                'next_text' => '<span>Next</span>',
+              ]); 
             ?>
           </div>
 
-          <div>
-            <h2 class="govuk-heading-m govuk-!-margin-bottom-2" data-testid="blog-post-title">
-              <a class="govuk-link" href="<?php the_permalink(); ?>" data-testid="blog-post-link" >
-                <?php the_title(); ?>
-              </a>
-            </h2>
+        <?php else : ?>
+          <p class="govuk-body">No Blog found.</p>
+        <?php endif; ?>
 
-            <p data-testid="blog-decs">
-              <?php echo esc_html(gca_clean_post_excerpt(320)); ?>
-            </p>
+      </div>
 
-            <p class="govuk-body">
-              By <?php echo esc_html(get_the_author()); ?>
-            </p>
-            
-            <div class="date_bottom" data-testid="blog-post-date">
-              <span class="govuk-!-margin-right-2" style="margin:0;">
-                <?php echo esc_html(get_the_date('j F Y')); ?>
-              </span>
-              <?php 
-              $terms = get_the_terms(get_the_ID(), 'label');
-
-              if ($terms && !is_wp_error($terms)) : 
-                $term = array_shift($terms); ?>
-                <span class="govuk-body-s tag_label location" style="margin:0;">
-                    <?php echo esc_html($term->name); ?>
-                </span>
-              <?php endif; ?>
-            </div>
-          </div>
-
-        </article>
-      <?php endwhile; ?>
-
-        <div class="govuk-!-margin-top-8 govuk-!-margin-bottom-8" data-testid="blog-pagination">
-          <?php 
-            the_posts_pagination( array(
-                'mid_size'  => 2,
-                'prev_text' => sprintf(
-                    '<span class="icon">
-                        <svg width="17" height="14" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M6.7 0l1.4 1.4-4.3 4.3h13v2H3.9l4.2 4-1.4 1.4L0 6.7z" fill="#007194" fill-rule="evenodd"/></svg>
-                      </span> <span>Previous</span>
-                    <span class="govuk-visually-hidden">page</span>'
-                ),
-                'next_text' => sprintf(
-                    '<span>Next</span> <span class="govuk-visually-hidden">page</span>
-                    <span class="icon">
-                        <svg width="17" height="14" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M10.1 0L8.7 1.4 13 5.7H0v2h12.9l-4.2 4 1.4 1.4 6.7-6.4z" fill="#007194" fill-rule="evenodd"/></svg>
-                    </span>'
-                ),
-            ) ); 
-          ?>
-        </div>
-
-      <?php else : ?>
-        <p class="govuk-body" data-testid="blog-no-posts">No Blog found.</p>
-      <?php endif; ?>
+    </div>
 
   </main>
 </div>
